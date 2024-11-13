9cdfcf9e5c28097edb156719b9e91fe2d40d5c8f
子悦解说 <ziyuecommentary@gmail.com>
2024-11-13 10:14:41 +0800

    喜报悲报

---
+++ README.md
@@ -0,0 +1,13 @@
+# “喜报悲报” 生成器
+
+这是一个在 C# 上用 [SkiaSharp](https://github.com/mono/SkiaSharp) 编写的 “喜报悲报” 生成器，支持 `\r` 和 `\n` 换行。
+
+欢迎帮助完善！
+
+## 示例
+
+![](example.png)
+
+## 同类作品
+
+* 《碧蓝档案》标题生成器：https://github.com/ZiYueCommentary/BlueArchiveLogo/
---
+++ Xibao.cs
@@ -0,0 +1,66 @@
+﻿using SkiaSharp;
+
+public static class Xibao
+{
+    public static void Main()
+    {
+        File.WriteAllBytes("output.png", Generate(true, "喜报\r第二行\n第三行"));
+    }
+
+    public static byte[] Generate(bool isXibao, string text)
+    {
+        SKTypeface face = SKTypeface.FromFile("HarmonyOS.ttf");
+        SKFont font = new SKFont(face, 100);
+        using SKSurface? surface = SKSurface.Create(new SKImageInfo(1024, 768));
+        SKCanvas? canvas = surface.Canvas;
+        SKPaint paint = new SKPaint()
+        {
+            IsAntialias = true,
+            Color = isXibao ? new SKColor(255, 10, 10) : new SKColor(0, 5, 0)
+        };
+        using SKBitmap halo = SKBitmap.Decode(isXibao ? "xibao.jpg" : "beibao.jpg");
+        canvas.DrawBitmap(halo, 0, 0, paint);
+        DrawCenteredText(canvas, text, 1024, 512, 384, font, paint);
+        using SKData? output = surface.Snapshot().Encode();
+        return output.ToArray();
+    }
+
+    public static void DrawCenteredText(SKCanvas canvas, string text, float width, float x, float y, SKFont font, SKPaint paint)
+    {
+        List<string> lines = [];
+        while (!string.IsNullOrEmpty(text))
+        {
+            if (text.StartsWith('\r') || text.StartsWith('\n'))
+            {
+                text = text[1..];
+                continue;
+            }
+
+            int breakIndex = font.BreakText(text, width, out _);
+            int rIndex = text.IndexOf('\r');
+            int nIndex = text.IndexOf('\n');
+            rIndex = rIndex == -1 ? int.MaxValue : rIndex;
+            nIndex = nIndex == -1 ? int.MaxValue : nIndex;
+            int feedIndex = Math.Min(rIndex, nIndex);
+
+            if (feedIndex < breakIndex)
+            {
+                lines.Add(text[..feedIndex]);
+                text = text[(feedIndex + 1)..];
+            }
+            else
+            {
+                lines.Add(text[..breakIndex]);
+                text = text[breakIndex..];
+            }
+        }
+
+        float height = lines.Count * font.Spacing;
+        float baselineY = y - height / 2 - font.Metrics.Ascent;
+        foreach (string line in lines)
+        {
+            canvas.DrawText(line, x, baselineY, SKTextAlign.Center, font, paint);
+            baselineY += font.Spacing;
+        }
+    }
+}
